d2b9624b67a373adae250434b213ad0a1a68eb4f
marian muzyka <mrjnmzk@gmail.com>
2020-09-11 10:47:44 +0300

    Update reducers.js

--- src/components/redux/reducers.js
+++ src/components/redux/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { createReducer } from "@reduxjs/toolkit";
+
 import { Type } from "./contactsActionsTypes";
 
 const contacsReducer = (state = [], { type, payload }) => {
